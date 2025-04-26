c286e7c054ab51dbc009c67e57db9ea12534c155
Lenka Michaleová <michaleova.lenka@gmail.com>
2025-04-26 21:07:00 +0200

    vyřešení třetí části úkolu

--- src/components/City/City.jsx
+++ src/components/City/City.jsx
@@ -1,7 +1,9 @@
-export const City = ({name, population, area}) => (    
+export const City = ({name, photo, population, area, district}) => (    
     <div key={name} className="city">
         <h2>{name}</h2>
+        <img src={photo} alt={name} />
         <p>Počet obyvatel: {population}</p>
         <p>Rozloha: {area} km3</p>
+        <p>Okres: {district}</p>
     </div>
 )
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,14 @@
 import { render } from '@czechitas/render';
 import { City } from '../components/City/City';
-import { cities } from './data';
+import { cities } from './cz-cities';
 import '../global.css';
 import './index.css';
 import '../components/City/City.css'
 
 document.querySelector('#root').innerHTML = render(
   <div className="container">
-    {cities.map(({name, population, area}) => (
-      <City name={name} population={population} area={area}/>
+    {cities.map(({name, photo, population, area, district}) => (
+      <City name={name} photo={photo} population={population} area={area} district={district}/>
     ))}
   </div>
 );
